131f4a01678d982b4e9a5ad3d92aaf0aaba3edda
chiluen <leo545035@gmail.com>
2021-01-16 00:27:37 +0800

    add ROUGE test code

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 ## Crawling data for CNN
     python3 get_data.py -num 3
 
-<br>
 
 ## Summarization demo
     python3 run.py -e 1
@@ -17,3 +16,7 @@
     wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-pytorch_model.bin
     wget https://huggingface.co/bert-base-uncased/resolve/main/vocab.txt
 
+
+## Testing for ROUGE
+* First download the CNN_Dailymail from tensorflow dataset (Can be done in ROUGE_test.ipynb)
+* Follow the steps in ROUGE_test.ipynb
--- model/extractive.py
+++ model/extractive.py
@@ -11,19 +11,25 @@ import tensorflow_hub as hub
 PRETRAINED_ROOT = "./pretrained_model"
 
 class bert_knn():
-    def __init__(self, model_name):
+    def __init__(self, model_name='distilbert-base-uncased'):
         self.model = Summarizer(model_name) #location at /.cache/torch
+    
     def summarize(self, text):
         num_sentences = int(input("How many sentences you want(1~30)\n"))
 
         summary = self.model(text, num_sentences=num_sentences)
         print(summary)
+    
+    def inference(self, text, num_sentences):
+        summary = self.model(text, num_sentences=num_sentences)
+        return summary
 
 
 #Use universal sentence encoder
 class textrank():
     def __init__(self):
         self.embed = hub.load(PRETRAINED_ROOT + "/universal-sentence-encoder_4/")
+    
     def summarize(self, text):
         sentences = sent_tokenize(text)
         word_embedding = np.array(self.embed(sentences))
@@ -37,10 +43,25 @@ class textrank():
 
         ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
 
-        num_of_sentences = int(input("How many sentences you want(1~30)\n"))   
-        summary = " ".join([i[1] for i in ranked_sentences[:num_of_sentences]])
+        num_sentences = int(input("How many sentences you want(1~30)\n"))   
+        summary = " ".join([i[1] for i in ranked_sentences[:num_sentences]])
         
         print(summary)
+
+    def inference(self, text, num_sentences):
+        sentences = sent_tokenize(text)
+        word_embedding = np.array(self.embed(sentences))
+
+        #generate cosine similarity matrix
+        sim_matrix = cosine_similarity(word_embedding)
+
+        #create graph and generate scores from pagerank algorithms
+        nx_graph = nx.from_numpy_array(sim_matrix)
+        scores = nx.pagerank(nx_graph)
+
+        ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True) 
+        summary = " ".join([i[1] for i in ranked_sentences[:num_sentences]])    
+        return summary 
 
 
 
