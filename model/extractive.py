bedfb4bb1e9612d83b3fb11f716e428edfc285b4
chiluen <leo545035@gmail.com>
2021-01-16 13:39:46 +0800

    add bert_embedding_sum

--- model/extractive.py
+++ model/extractive.py
@@ -7,6 +7,8 @@ import networkx as nx
 import numpy as np
 from nltk.tokenize import sent_tokenize  #需要nltk.download('punkt')
 import tensorflow_hub as hub
+from transformers import BertTokenizer, BertModel
+import torch
 
 PRETRAINED_ROOT = "./pretrained_model"
 
@@ -57,7 +59,60 @@ class textrank():
 
         #create graph and generate scores from pagerank algorithms
         nx_graph = nx.from_numpy_array(sim_matrix)
+        try:
+            scores = nx.pagerank(nx_graph, tol=1, max_iter=10000)
+        except:
+            print("One fail")
+            return None
+
+        ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True) 
+        summary = " ".join([i[1] for i in ranked_sentences[:num_sentences]])    
+        return summary 
+
+#Use bert token encoder with word embedding summation
+class textrank_bert():
+    def __init__(self):
+
+        self.tokenizer = BertTokenizer.from_pretrained(PRETRAINED_ROOT + "/bert/")
+        self.embed = BertModel.from_pretrained(PRETRAINED_ROOT + "/bert/")
+        #self.embed = hub.load(PRETRAINED_ROOT + "/bert/")
+    
+    def summarize(self, text):
+        sentences = sent_tokenize(text)
+        tokens = self.tokenizer(sentences, padding=True, return_tensors="pt") #做padding
+        word_embedding = self.embed(**tokens)
+        word_embedding = torch.sum(word_embedding[0], dim=1).detach().numpy() #對每一句畫的所有token做summation
+
+        #針對每一個句子, 做summation
+        sim_matrix = cosine_similarity(word_embedding)
+
+        #create graph and generate scores from pagerank algorithms
+        nx_graph = nx.from_numpy_array(sim_matrix)
         scores = nx.pagerank(nx_graph)
+
+        ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
+        
+        num_sentences = int(input("How many sentences you want(1~30)\n")) 
+            
+        summary = " ".join([i[1] for i in ranked_sentences[:num_sentences]])
+        print(summary)
+
+    def inference(self, text, num_sentences):
+        sentences = sent_tokenize(text)
+        tokens = self.tokenizer(sentences, padding=True, return_tensors="pt") #做padding
+        word_embedding = self.embed(**tokens)
+        word_embedding = torch.sum(word_embedding[0], dim=1).detach().numpy() #對每一句畫的所有token做summation
+
+        #generate cosine similarity matrix
+        sim_matrix = cosine_similarity(word_embedding)
+
+        #create graph and generate scores from pagerank algorithms
+        nx_graph = nx.from_numpy_array(sim_matrix)
+        try:
+            scores = nx.pagerank(nx_graph, tol=1, max_iter=10000)
+        except:
+            print("One fail")
+            return None
 
         ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True) 
         summary = " ".join([i[1] for i in ranked_sentences[:num_sentences]])    
